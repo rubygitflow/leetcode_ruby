59159b16440bc7590214c90ade97b28398985d95
rubygitflow <nrubygitflow@gmail.com>
2024-05-21 03:07:03 +0600

    add comments

--- maximum_length_of_semi_decreasing_subarrays.rb
+++ maximum_length_of_semi_decreasing_subarrays.rb
@@ -9,6 +9,7 @@ def max_subarray_length_dec(nums)
   d = Hash.new { |h, k| h[k] = [] }
   nums.each_with_index {|x, i| d[x] << i}
   ans, k = 0, INF
+  # Decreasing array
   d.keys.sort {|a,b| b <=> a}.each do |x|
     ans = [ans, d[x][-1] - k + 1].max
     k = [k, d[x][0]].min
@@ -22,6 +23,7 @@ def max_subarray_length_inc(nums)
   d = Hash.new { |h, k| h[k] = [] }
   nums.each_with_index {|x, i| d[x] << i}
   ans, k = 0, INF
+  # Increasing array
   d.keys.sort.each do |x|
     ans = [ans, d[x][-1] - k + 1].max
     k = [k, d[x][0]].min
@@ -44,7 +46,7 @@ p max_subarray_length_dec([10,12,12,11,11])
 p("Semi-Increasing Subarrays")
 p max_subarray_length_inc([11,10,6,1,2,3,4,5,6,7])
 # Output: 8
-p max_subarray_length_inc([63,59,64,60,63,55,57,50,60,61,58,])
+p max_subarray_length_inc([63,59,64,60,63,55,57,50,60,61,58])
 # Output: 9
 p max_subarray_length_inc([57,55,50,60,61,58,63,59,64,58,63])
 # Output: 11
