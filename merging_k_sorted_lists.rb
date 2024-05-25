5731ffe95c106e629c2a2eaa0f34a7b21b3b0e61
rubygitflow <nrubygitflow@gmail.com>
2024-05-26 05:37:19 +0600

    add Merge k Sorted Lists

--- merging_k_sorted_lists.rb
+++ merging_k_sorted_lists.rb
@@ -14,17 +14,38 @@ require_relative 'list_node'
 # @param {ListNode[]} lists
 # @return {ListNode}
 def merge_k_lists(lists)
-  lst = lists.map do |list|
+  bundle = lists.map do |list|
     add_linked_list(list)
   end
-  return [] if lst.size.zero?
+  # create output
+  head = ListNode.new(nil)
+  return [] if bundle.size.zero?
 
-  lst
+  # rename output for loop
+  tail = head
+  # Remove empty list first
+  bundle.delete_if { |list| list.nil? }
+  while !bundle.empty?
+    # Sort bundle from min to max
+    bundle.sort_by! { |list| list.val }
+    # add next item to output
+    tail.next = ListNode.new(bundle[0].val)
+    # replace linked-list's item in output
+    tail = tail.next
+    # move bundle's item
+    bundle[0] = bundle[0].next
+    # Remove empty list if exists
+    bundle.delete_if { |list| list.nil? }
+  end
+
+  head.next.nil? ? ListNode.new(nil) : head.next
 end
 
-p merge_k_lists([[1,4,5],[1,3,4],[2,6]])
+puts merge_k_lists([[1,4,5],[1,3,4],[2,6]]).to_s
 # Output: [1,1,2,3,4,4,5,6]
-p merge_k_lists([])
+puts merge_k_lists([]).to_s
 # Output: []
-p merge_k_lists([[]])
+puts merge_k_lists([[]]).to_s
 # Output: []
+puts merge_k_lists([[11,14,15],[21,23,24],[2,6]]).to_s
+# Output: [2,6,11,14,15,21,23,24]
