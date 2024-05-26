578fbb0b108a8e2127c16a4948dfa24c4abe50e8
rubygitflow <nrubygitflow@gmail.com>
2024-05-26 20:21:14 +0600

    add Alternating Digit Sum

---
+++ alternating_digit_sum.rb
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/alternating-digit-sum/description/
+# 2544. Alternating Digit Sum
+
+# @param {Integer} n
+# @return {Integer}
+def alternate_digit_sum(n)
+  pos = -1
+  n.to_s.chars.reduce(0) {|res,n| pos *= -1; res + n.to_i * pos }
+end
+
+p alternate_digit_sum(521)
+# Output: 4
+p alternate_digit_sum(111)
+# Output: 1
+p alternate_digit_sum(886996)
+# Output: 0
