fc0f6f0400b70375b323ef6a4a90afa6ab2b45e2
rubygitflow <nrubygitflow@gmail.com>
2024-05-26 17:23:13 +0600

    add K-diff Pairs in an Array

--- count_number_of_pairs_with_absolute_difference_k.rb
+++ count_number_of_pairs_with_absolute_difference_k.rb
@@ -15,6 +15,7 @@ def count_k_difference(nums, k)
   output
 end
 
+puts "Count Number of Pairs With Absolute Difference K"
 p count_k_difference([1,2,2,1], 1)
 # Output: 4
 p count_k_difference([1,2,2,1,1], 1)
@@ -25,3 +26,32 @@ p count_k_difference([1,3], 3)
 # Output: 0
 p count_k_difference([3,2,1,5,4], 2)
 # Output: 3
+
+
+#######################
+# https://leetcode.com/problems/k-diff-pairs-in-an-array/description/
+# 532. K-diff Pairs in an Array
+# UNIQUE k-diff pairs !!!
+
+require 'set'
+
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer}
+def find_pairs(nums, k)
+  backside, output = Set.new, Set.new
+  nums.each do |num|
+    output << num - k if backside === num - k
+    output << num if backside === num + k
+    backside.add(num)
+  end
+  output.size
+end
+
+puts "K-diff unique Pairs in an Array"
+p find_pairs([3,1,4,1,5], 2)
+# Output: 2
+p find_pairs([1,2,3,4,5], 1)
+# Output: 4
+p find_pairs([1,3,1,5,4], 0)
+# Output: 1
