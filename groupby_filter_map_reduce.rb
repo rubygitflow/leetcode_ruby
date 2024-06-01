d09a2ef7b74b0cb1c3ac1386ff37f5911c4fe640
rubygitflow <nrubygitflow@gmail.com>
2024-06-01 06:44:40 +0600

    add Filter Elements from Array

--- groupby_filter_map_reduce.rb
+++ groupby_filter_map_reduce.rb
@@ -71,7 +71,7 @@
 
 class Array
 # module Enumerable
-  # @param {Proc} fn
+  # @param {lambda} fn
   # @return {Hash}
   # def group_by(&fn)
   #   return {} unless block_given?
@@ -132,10 +132,81 @@ pp [
 ].group_by
 # Output: 
 # {}
-
 pp [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].group_by(&-> (n) { (n > 5).to_s } )
 # Output:
 # {
 #   "false" => [1, 2, 3, 4, 5],
 #   "true" => [6, 7, 8, 9, 10]
 # }
+
+
+#######################
+# https://leetcode.com/problems/filter-elements-from-array/description/
+# 2634. Filter Elements from Array
+
+# Please solve it without .filter method.
+# https://ruby-doc.org/3.3.0/Enumerable.html#method-i-filter
+# https://ruby-doc.org/3.3.0/Array.html#method-i-select
+# https://ruby-doc.org/3.3.0/Array.html#method-i-filter
+# https://ruby-doc.org/3.3.0/Hash.html#method-i-filter
+# Look at https://docs.ruby-lang.org/en/master/Proc.html
+
+# Example 1:
+# Input: arr = [0,10,20,30], fn = function greaterThan10(n) { return n > 10; }
+# Output: [20,30]
+# Explanation:
+# const newArray = filter(arr, fn); // [20, 30]
+# The function filters out values that are not greater than 10
+
+# Example 2:
+# Input: arr = [1,2,3], fn = function firstIndex(n, i) { return i === 0; }
+# Output: [1]
+# Explanation:
+# fn can also accept the index of each element
+# In this case, the function removes elements not at index 0
+
+# Example 3:
+# Input: arr = [-2,-1,0,1,2], fn = function plusOne(n) { return n + 1 }
+# Output: [-2,0,1,2]
+# Explanation:
+# Falsey values such as 0 should be filtered out
+
+class Array
+# module Enumerable
+  # @param {Proc} fn
+  # @return {Array}
+  # def filter(&fn)
+  #   return [] unless block_given?
+
+  #   each_with_object([]).with_index do |(elem, res), i|
+  #     res << elem if fn.call(elem, i)
+  #   end
+  # end
+  def filter
+    return [] unless block_given?
+
+    each_with_object([]).with_index do |(elem, res), i|
+      res << elem if yield(elem, i)
+    end
+  end
+end
+
+puts "Filter Elements from Array"
+pp [0,10,20,30].filter
+# Output:
+# []
+# pp [0,10,20,30].filter(&-> (n) { |n| n > 5 } )
+# returns an error (ArgumentError):
+# "wrong number of arguments (given 2, expected 1)"
+pp [0,10,20,30].filter(&Proc.new{ |n| n > 5 } )
+# Output:
+# [20,30]
+pp [0,10,20,30].filter { |n| n > 5 }
+# Output:
+# [20,30]
+pp [1,2,3].filter(&Proc.new{ |n, i| i == 0 } )
+# Output:
+# [1]
+pp [-2,-1,0,1,2].filter(&Proc.new{ |n| n + 1 != 0 } )
+# Output:
+# [-2,0,1,2]
