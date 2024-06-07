e50ebb9c2a70421f30e54fcb245e1d3ce93f4b81
rubygitflow <nrubygitflow@gmail.com>
2024-06-07 21:31:34 +0600

    refactor Calculate Digit Sum of a String

--- calculate_digit_sum_of_string.rb
+++ calculate_digit_sum_of_string.rb
@@ -5,16 +5,31 @@
 # @param {Integer} k
 # @return {String}
 def digit_sum(s, k)
-  while s.size > 3
-    col, s = s.chars.each_with_object([[], '']) do |el, out|
-      out[0] << el.to_i
-      if out[0].size == k
-        out[1] << out[0].sum.to_s
-        out[0].clear
+  while s.size > k
+    col, s = s.chars.each_with_object([[], '']) do |el, accum|
+      accum[0] << el.to_i
+      if accum[0].size == k
+        accum[1] << accum[0].sum.to_s
+        accum[0].clear
       end
-      out
     end
     s += col.sum.to_s if col.size > 0
+  end
+  s
+end
+
+# @param {String} s
+# @param {Integer} k
+# @return {String}
+def digit_sum_ii(s, k)
+  accum = []
+  while s.size > k
+    a = s.split("").map(&:to_i)
+    accum.clear
+    (0...a.size).step(k).each do |el|
+      accum << a.slice(el,k).sum
+    end
+    s = accum.join("")
   end
   s
 end
@@ -22,4 +37,9 @@ end
 p digit_sum("11111222223", 3)
 # Output: "135"
 p digit_sum("00000000", 3)
+# Output: "000"
+
+p digit_sum_ii("11111222223", 3)
+# Output: "135"
+p digit_sum_ii("00000000", 3)
 # Output: "000"
