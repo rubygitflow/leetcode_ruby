34699eeb2e629476e4c8cc6f45252d52928f677a
rubygitflow <nrubygitflow@gmail.com>
2024-06-09 03:47:00 +0600

    add a test case

--- minimum_bit_flips.rb
+++ minimum_bit_flips.rb
@@ -15,3 +15,20 @@ p min_bit_flips(3, 4)
 # Output: 3
 p min_bit_flips(10**9, 1)
 # Output: 14
+
+def min_bit_flips_ii(start, goal)
+  tmp = start ^ goal
+  out = 0
+  while tmp > 0
+      out += tmp & 1
+      tmp >>= 1
+  end
+  out
+end
+
+p min_bit_flips_ii(10, 7)
+# Output: 3
+p min_bit_flips_ii(3, 4)
+# Output: 3
+p min_bit_flips_ii(10**9, 1)
+# Output: 14
