f0c991d8a8a0b26a568908adf65cff9a8ee4f95a
rubygitflow <nrubygitflow@gmail.com>
2024-06-10 15:00:27 +0600

    add a test case

--- find_triangular_sum.rb
+++ find_triangular_sum.rb
@@ -22,3 +22,21 @@ p triangular_sum([1,2,3,4,5])
 # Output: 8
 p triangular_sum([5])
 # Output: 5
+
+# @param {Integer[]} nums
+# @return {Integer}
+def triangular_sum_ii(nums)
+  res = nums.clone # nums.dup
+  while res.size > 1
+    tmp = []
+    # https://ruby-doc.org/3.3.0/Enumerable.html#method-i-each_cons
+    res.each_cons(2) { |pair| tmp << (pair.sum % 10) }
+    res.replace(tmp)
+  end
+  res.first
+end
+
+p triangular_sum_ii([1,2,3,4,5])
+# Output: 8
+p triangular_sum_ii([5])
+# Output: 5
