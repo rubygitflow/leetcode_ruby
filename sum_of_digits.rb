3cda11b6657033c47d297e97ae609688aee98b92
rubygitflow <nrubygitflow@gmail.com>
2024-06-16 17:43:50 +0600

    add Happy Number

--- sum_of_digits.rb
+++ sum_of_digits.rb
@@ -11,9 +11,30 @@ def get_lucky(s, k)
   tmp
 end
 
+puts "Sum of Digits of String After Convert"
 p get_lucky("iiii", 1)
 # Output: 36
 p get_lucky("leetcode", 2)
 # Output: 6
 p get_lucky("zbax", 2)
 # Output: 8
+
+
+########################
+# https://leetcode.com/problems/happy-number/description/
+# 202. Happy Number
+
+# @param {Integer} n
+# @return {Boolean}
+def is_happy(n)
+  while n>9
+    n = n.digits.reduce(0) { _1 + _2 * _2 }
+  end
+  n == 1
+end
+
+puts "Happy Number"
+p is_happy(19)
+# Output: true
+p is_happy(2)
+# Output: false
