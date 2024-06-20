7f73c38bb4391d9b1959191992d9e6abdc293d0f
rubygitflow <nrubygitflow@gmail.com>
2024-06-20 06:33:02 +0600

    refactor Longest Common Prefix

--- common_prefix.rb
+++ common_prefix.rb
@@ -4,6 +4,8 @@
 # @param {String[]} strs
 # @return {String}
 def longest_common_prefix(strs)
+  return "" if strs.size < 2
+
   strs[0].chars.each_with_index do |char, i|
     strs[1..].each do |word|
       return strs[0][0...i] if word.length<=i || word[i] != char
@@ -20,3 +22,5 @@ p(longest_common_prefix(["dog","racecar","car"]))
 # Output: ""
 p(longest_common_prefix(["flow","flow","flow"]))
 # Output: "flow"
+p(longest_common_prefix(["dog"]))
+# Output: ""
