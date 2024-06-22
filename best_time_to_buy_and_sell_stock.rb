b5ccf69b61b8fc4972dc99031903dc94f1e6a626
rubygitflow <nrubygitflow@gmail.com>
2024-06-23 01:20:41 +0600

    add Best Time to Buy and Sell Stock II

--- best_time_to_buy_and_sell_stock.rb
+++ best_time_to_buy_and_sell_stock.rb
@@ -24,3 +24,28 @@ p(max_profit([7,1,5,3,6,4]))
 # Output: 5
 p(max_profit([7,6,4,3,1]))
 # Output: 0
+
+
+#######################
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+# 122. Best Time to Buy and Sell Stock II
+
+
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit_ii(prices)
+  prices.each_cons(2).reduce(0) { |max_price, (prev, curr)| max_price + [curr - prev, 0].max }
+end
+
+
+puts("Best Time to Buy and Sell Stock II")
+p(max_profit_ii([7,1,5,3,6,4]))
+# Output: 7
+p(max_profit_ii([1,2,3,4,5]))
+# Output: 4
+p(max_profit_ii([7,6,4,3,1]))
+# Output: 0
+p(max_profit_ii([7]))
+# Output: 0
+p(max_profit_ii([]))
+# Output: 0
