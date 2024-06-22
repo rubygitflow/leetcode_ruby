dbac8254fe96d2d08b96c1dac25e27b8363abb46
rubygitflow <nrubygitflow@gmail.com>
2024-06-23 04:34:48 +0600

    add Best Time to Buy and Sell Stock III

--- best_time_to_buy_and_sell_stock.rb
+++ best_time_to_buy_and_sell_stock.rb
@@ -70,3 +70,35 @@ p(max_profit_ii_ex([7]))
 # Output: 0
 p(max_profit_ii_ex([]))
 # Output: 0
+
+
+#######################
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/description/
+# 123. Best Time to Buy and Sell Stock III
+
+
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit_iii(prices)
+  return 0 if prices.size < 2
+
+  prices.reduce([[0], prices[0]]) do |rising_trans, curr|
+    if curr > rising_trans[1]
+      rising_trans[0][-1] += curr - rising_trans[1]
+      [rising_trans[0] , curr]
+    elsif curr < rising_trans[1]
+      [rising_trans[0].push(0), curr]
+    else
+      rising_trans
+    end
+  end.fetch(0).max(2).sum
+end
+
+
+puts("Best Time to Buy and Sell Stock III")
+p(max_profit_iii([3,3,5,0,0,3,1,4]))
+# Output: 6
+p(max_profit_iii([1,2,3,4,5]))
+# Output: 4
+p(max_profit_iii([7,6,4,3,1]))
+# Output: 0
