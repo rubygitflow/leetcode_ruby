d26b0cf6e5516583276e0932482ca170b7c3704b
rubygitflow <nrubygitflow@gmail.com>
2024-06-25 05:42:45 +0600

    add Single Number II

--- single_number.rb
+++ single_number.rb
@@ -17,3 +17,23 @@ p(single_number([4,1,2,1,2]))
 # Output: 4
 p(single_number([1]))
 # Output: 1
+
+
+#######################
+# https://leetcode.com/problems/single-number-ii/description/
+# 137. Single Number II
+
+
+# @param {Integer[]} nums
+# @return {Integer}
+def single_number_ii(nums)
+  nums.tally.find {|k, v| v == 1 }&.first || 0
+end
+
+puts("Single Number II")
+p(single_number_ii([2,2,3,2]))
+# Output: 3
+p(single_number_ii([0,1,0,1,0,1,99]))
+# Output: 99
+p(single_number_ii([0,1,0,1,0,1]))
+# Output: 0
